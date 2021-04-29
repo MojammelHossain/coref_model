14ad51b06eb75cc76ebbbb794558adc8731d79ae
MojammelHossain <mojammel.hossain@northsouth.edu>
2021-04-29 15:54:53 +0600

    fix indentation cause cuda out of memory

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ logging.basicConfig(format=format)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+
 if __name__ == "__main__":
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
     if torch.cuda.is_available():
@@ -44,35 +45,24 @@ if __name__ == "__main__":
             out, loss = model.forward(batch['input_ids'].to(device), batch['input_mask'].to(device), batch['text_len'],
                                       batch['speaker_ids'].to(device), batch['genre'], True, batch['gold_starts'].to(device),
                                       batch['gold_ends'].to(device), batch['cluster_ids'].to(device), batch['sentence_map'].to(device))
-        total_loss += loss.item()
-        loss.backward()
-        bert_optimizer.step()
-        task_optimizer.step()
-        global_steps += 1
+            
+            total_loss += loss.item()
+            loss.backward()
+            bert_optimizer.step()
+            task_optimizer.step()
+            global_steps += 1
+            
+            if global_steps % config['report_frequency'] == 0:
+                total_time = time.time() - initial_time
+                steps_per_second = global_steps/total_time
+                avg_loss = total_loss/config['report_frequency']
+                logger.info("[{}] loss={:.2f}, steps/s={:.2f}".format(global_steps, avg_loss, steps_per_second))
+                total_loss = 0.0
 
-        if global_steps % config['report_frequency'] == 0:
-            total_time = time.time() - initial_time
-            steps_per_second = global_steps/total_time
+            if global_steps > 0 and global_steps % config['eval_frequency'] == 0:
+                eval_f1 = evaluate.evaluate(model, config, device)
 
-            avg_loss = total_loss/config['report_frequency']
-            logger.info("[{}] loss={:.2f}, steps/s={:.2f}".format(global_steps, avg_loss, steps_per_second))
-            total_loss = 0.0
-
-        if global_steps > 0 and global_steps % config['eval_frequency'] == 0:
-            eval_f1 = evaluate.evaluate(model, config, device)
-
-            path = config["log_dir"]+"/model.{}.pt".format(global_steps)
-            torch.save({
-                'eval_f1': eval_f1,
-                'max_f1' : max_f1,
-                'global_steps': global_steps,
-                'model': model.state_dict(),
-                'bert_optimizer': bert_optimizer.state_dict(),
-                'task_optimizer': task_optimizer.state_dict()
-                }, path)
-
-            if eval_f1 > max_f1:
-                max_f1 = eval_f1
+                path = config["log_dir"]+"/model.{}.pt".format(global_steps)
                 torch.save({
                     'eval_f1': eval_f1,
                     'max_f1' : max_f1,
@@ -80,4 +70,15 @@ if __name__ == "__main__":
                     'model': model.state_dict(),
                     'bert_optimizer': bert_optimizer.state_dict(),
                     'task_optimizer': task_optimizer.state_dict()
-                    }, (config["log_dir"]+"/model.max.pt"))
+                    }, path)
+
+                if eval_f1 > max_f1:
+                    max_f1 = eval_f1
+                    torch.save({
+                        'eval_f1': eval_f1,
+                        'max_f1' : max_f1,
+                        'global_steps': global_steps,
+                        'model': model.state_dict(),
+                        'bert_optimizer': bert_optimizer.state_dict(),
+                        'task_optimizer': task_optimizer.state_dict()
+                        }, (config["log_dir"]+"/model.max.pt"))
